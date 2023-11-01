ea5ed4d9b1ac5950bc90c6557aae33f89d709689
ARMANDO BUGARIN <armando.bugarin24@gmail.com>
2023-10-31 21:53:10 -0700

    completed app.js, need to edit last objects

--- app.js
+++ app.js
@@ -1,47 +1,119 @@
-`script`;
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-let seattle = {
+const seattle = {
+    location: 'Seattle',
     minCustomers: 23,
     maxCustomers: 65,
-    avgCookieSale: 6.3,
-    custEachHour: [],
-    getCustPerHour: function() {
-        for (let i = 6; i <= storeHours.length; i++) {  // i=hour
-            let customers = random(this.minCustomers, this.maxCustomers);
-        }
-    }
+    avgCookiesSale: 6.3,
+    sales: [],
+    estimate: function () {
+        this.sales = estimateSales(this);
+    },
 }
 
-let storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
+// initialize sales
+seattle.estimate();
 
-function random(min, max) {
+
+function estimateSales(store) {
+    const sales = [];
+    for (let i = 0; i < hours.length; i++) {
+        const numCustomers = randomInRange(store.minCustomers, store.maxCustomers);
+        console.log(numCustomers);
+        const hourSales = Math.ceil(numCustomers * store.avgCookiesSale);
+        sales.push(hourSales);
+    }
+    return sales;
+}
+
+function randomInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+}
 
 
-let tokyo = {
+let container = document.getElementById('root'); // link to sales.html
+
+function render(store) {   //how to copy coding from seattle to others, need to take out hard code
+
+    // need an article per cookie stand
+    let cookieStandArticle = document.createElement('article');
+    container.appendChild(cookieStandArticle);
+
+    let heading = document.createElement('h2');
+    cookieStandArticle.appendChild(heading);
+    heading.textContent = seattle.location;
+
+    let hoursList = document.createElement('ul');
+    cookieStandArticle.appendChild(hoursList);
+
+    let totalSold = 0;
+
+    for (let i = 0; i < seattle.sales.length; i++) {
+        let salesItem = document.createElement('li');
+        hoursList.appendChild(salesItem);
+        let cookiesSoldThisHour = seattle.sales[i];
+        let salesInfo = `${hours[i]}: ${seattle.sales[i]} cookies`;
+        salesItem.textContent = salesInfo
+    }
+
+    // add total line
+    const totalItem = document.createElement('li');
+    hoursList.appendChild(totalItem);
+    const totalInfo = `Total: ${hoursList} cookies sold`; // need to find value for "${hoursList}"
+    totalItem.textContent = totalInfo;
+}
+
+render(seattle);
+
+
+const tokyo = {
     minCustomer: 3,
     maxCustomer: 24,
-    avgCookieSale: 1.2,
+    avgCookiesSale: 1.2,
+    sales: [],
+    estimate: function () {
+        this.sales = estimateSales(this);
+    },
 }
+ 
+render(tokyo);
+tokyo.estimate();
 
 
-let dubai = {
+const dubai = {
     minCustomer: 11,
     maxCustomer: 38,
-    avgCookieSale: 3.7,
+    avgCookiesSale: 3.7,
+    sales: [],
+    estimate: function () {
+        this.sales = estimateSales(this);
+    },
 }
+render(dubai);
+dubai.estimate();
 
 
-let paris = {
+const paris = {
     minCustomer: 20,
     maxCustomer: 38,
-    avgCookieSale: 2.3
+    avgCookiesSale: 2.3,
+    sales: [],
+    estimate: function () {
+        this.sales = estimateSales(this);
+    },
 }
+render(paris);
+paris.estimate();
 
 
-let lima = {
+const lima = {
     minCustomer: 2,
     maxCustomer: 16,
-    avgCookieSale: 4.6
+    avgCookiesSale: 4.6,
+    sales: [],
+    estimate: function () {
+        this.sales = estimateSales(this);
+    },
 }
+render(lima);
+lima.estimate();
